9065b24a6213994de68fd53fe39c8aa81c8ded27
Fuat Erbey <fuaterbey@gmail.com>
2026-04-09 16:03:02 +0300

    task-1 file is completed.

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,20 @@
-z
+function isEnoughCapacity(products, containerSize) {
+
+    const pruductValues = Object.values(products);
+    
+    let totalPruductValue = 0;
+ 
+    for (const value of pruductValues) {
+        totalPruductValue += value;
+    }
+
+    if (totalPruductValue <= containerSize) {
+        return true;
+    }
+
+    else {
+        return false;
+        }    
+}
+
+console.log(isEnoughCapacity ({ apples: 2, grapes: 3, carrots: 1 }, 8));
