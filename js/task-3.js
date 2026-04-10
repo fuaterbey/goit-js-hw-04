99c7a132f8480d5e2acf1373f921cddbcc1a8890
Fuat Erbey <fuaterbey@gmail.com>
2026-04-11 02:45:08 +0300

    this (key) is added.

--- js/task-3.js
+++ js/task-3.js
@@ -3,16 +3,16 @@ const profile = {
   playTime: 300,
 
     changeUsername(newName) {
-      profile.username = newName;
+      this.username = newName;
     },
    
     updatePlayTime(hours) {
-        profile.playTime = profile.playTime + hours;
+        this.playTime = this.playTime + hours;
     },
 
     getInfo() {
-      return `${profile.username} has ${profile.playTime} active hours!`
-    },
-    
+      return `${this.username} has ${this.playTime} active hours!`
+    },  
 
 };
+
